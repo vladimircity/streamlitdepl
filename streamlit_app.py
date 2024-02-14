fdb4954d21b0fa0c28102c7023254463809ca00a
vladimircity <vladimircity@gmail.com>
2024-02-14 13:23:55 +0200

    blank pixel

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,76 +1,83 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-import pandas as pd
+import gspread
+from pandas import read_csv
+from datetime import datetime
+
+
+CREDENTIALS = {
+        "type" : st.secrets["gcp_service_account"]["type"],
+        "project_id" : st.secrets["gcp_service_account"]["project_id"],
+        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+        "private_key" : st.secrets["gcp_service_account"]["private_key"],
+        "client_email" : st.secrets["gcp_service_account"]["client_email"],
+        "client_id" : st.secrets["gcp_service_account"]["client_id"],
+        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+    }
+
+
+if 'gsheet' not in st.session_state:
+    gclient = gspread.service_account_from_dict(CREDENTIALS)
+    gwb = gclient.open('Kuzov Sales')
+    gws = gwb.worksheet('Orders2')
+    print('gws.id =', gws.id)
+    st.session_state.gsheet = gws
+
+goods = read_csv('price.csv')[['Артикул', 'Назва', 'База']]
+
+custom_product = "Ввести вручну..."
+custom_manager = "Інший"
+PRODUCTS = ['', custom_product] + goods['Назва'].values.tolist()
 
 # Display Title and Description
-st.title("Vendor Management Portal")
-st.markdown("Enter the details of the new vendor below.")
+st.header("Кузов-Центр: створити замовлення")
 
-# Establishing a Google Sheets connection
-conn = st.connection("gsheets", type=GSheetsConnection)
+MANAGERS = ["Віталій", "Сергій", "Тарас", "Інший"]
 
-# Fetch existing vendors data
-existing_data = conn.read(worksheet="Vendors", usecols=list(range(6)), ttl=5)
-existing_data = existing_data.dropna(how="all")
+manager = st.radio("Менеджер:", MANAGERS, index=None, horizontal=True)
+if manager == custom_manager: 
+    manager = st.text_input("Введіть менеджера:", key='manager_key')
 
-# List of Business Types and Products
-BUSINESS_TYPES = [
-    "Manufacturer",
-    "Distributor",
-    "Wholesaler",
-    "Retailer",
-    "Service Provider",
-]
-PRODUCTS = [
-    "Electronics",
-    "Apparel",
-    "Groceries",
-    "Software",
-    "Other",
-]
 
-# Onboarding New Vendor Form
-with st.form(key="vendor_form"):
-    company_name = st.text_input(label="Company Name*")
-    business_type = st.selectbox("Business Type*", options=BUSINESS_TYPES, index=None)
-    products = st.multiselect("Products Offered", options=PRODUCTS)
-    years_in_business = st.slider("Years in Business", 0, 50, 5)
-    onboarding_date = st.date_input(label="Onboarding Date")
-    additional_info = st.text_area(label="Additional Notes")
+product = st.selectbox("Товар:", options=PRODUCTS, key='product_key')
+if product == custom_product: 
+    product = st.text_input("Введіть назву товару:")
 
-    # Mark mandatory fields
-    st.markdown("**required*")
+price = st.text_input(label="Ціна", key='price_key')
+price = int(price) if price else price
+quantity = st.selectbox("Кількість", options=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], index=0, key='quantity_key')
+customer = st.text_input(label="Клієнт")
+notes = st.text_input(label="Нотатки")
 
-    submit_button = st.form_submit_button(label="Submit Vendor Details")
 
-    # If the submit button is pressed
-    if submit_button:
-        # Check if all mandatory fields are filled
-        if not company_name or not business_type:
-            st.warning("Ensure all mandatory fields are filled.")
-            st.stop()
-        elif existing_data["CompanyName"].str.contains(company_name).any():
-            st.warning("A vendor with this company name already exists.")
-            st.stop()
-        else:
-            # Create a new row of vendor data
-            vendor_data = pd.DataFrame(
-                [
-                    {
-                        "CompanyName": company_name,
-                        "BusinessType": business_type,
-                        "Products": ", ".join(products),
-                        "YearsInBusiness": years_in_business,
-                        "OnboardingDate": onboarding_date.strftime("%Y-%m-%d"),
-                        "AdditionalInfo": additional_info,
-                    }
-                ]
-            )
 
-            # Add the new vendor data to the existing data
-            updated_df = pd.concat([existing_data, vendor_data], ignore_index=True)
+def reset():
+    values_list = [datetime.now().strftime("%d-%m-%Y %H:%M:%S"), manager, 13777, product, 50, price, quantity, customer, notes]
+    save(values_list)
+    st.session_state.product_key = ''
+    st.session_state.price_key = ''
+    st.session_state.quantity_key = 1
 
-            # Update Google Sheets with the new vendor data
-            conn.update(worksheet="Vendors", data=updated_df)
 
-            st.success("Vendor details successfully submitted!")
+if not product or not manager:
+    if 'success' not in st.session_state:
+        st.session_state.success = False
+        st.warning('Заповніть поля менеджер і товар')
+    elif st.session_state.success:
+        st.success('Товар успішно внесено')
+        st.warning('Заповніть наступний товар')
+    else:
+        st.warning('Заповніть поля менеджер і товар')
+else:
+    button = st.button('Внести', on_click=reset, use_container_width=False, type='primary')
+    # rate = st.radio(" ", [''], disabled=True)
+    st.write('')
+    st.session_state.success = True
+
+
+def save(values_list):
+    print(values_list)
+    st.session_state.gsheet.append_row(values_list, value_input_option='USER_ENTERED')
+   
