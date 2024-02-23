8a911217ece288064712f8f859154cb7648b27d3
vladimircity <vladimircity@gmail.com>
2024-02-23 13:45:30 +0200

    quantity number

--- testform.py
+++ testform.py
@@ -30,13 +30,9 @@ if product == custom_product:
 
 price = st.text_input(label='Ціна', key='price_key')
 price = int(price) if price else price
-quantity = st.selectbox(
-    'Кількість',
-    options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-             12, 13, 14, 15, 16, 17, 18, 19, 20],
-    index=0,
-    key='quantity_key',
-)
+
+quantity = st.number_input("Кількість", min_value=1, value=1)
+
 customer = st.text_input(label='Клієнт')
 notes = st.text_input(label='Нотатки')
 
@@ -77,9 +73,11 @@ else:
         'Внести', on_click=send_form, use_container_width=False, type='primary'
     )
 
-    st.write("Подивитись всі [ЗАМОВЛЕННЯ](https://docs.google.com/spreadsheets/d/1ThyJ0uPa3UNB1Yh1jh6EKIP2OutjFPX2F-GTypyBNYM/edit#gid=139450348)")
+    st.write("")
     st.session_state.success = True
 
+st.write("Подивитись всі [ЗАМОВЛЕННЯ](https://docs.google.com/spreadsheets/d/1ThyJ0uPa3UNB1Yh1jh6EKIP2OutjFPX2F-GTypyBNYM/edit#gid=139450348)")
+    
 
 def get_product_info(product):
     articul = ''
