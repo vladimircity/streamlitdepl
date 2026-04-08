08cb457843e209bdfa37380955feb6123bb75699
vladimircity <vladimircity@gmail.com>
2026-04-08 18:58:33 +0300

    q1

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ notes = st.text_input(label='Нотатки')
 def prepare_products():
     if not "Комплект:" in product:
         articul, base_price = get_product_info(product)
-        # total = price * quantity
-        total = quantity
+        quantity = 1
+        total = price * quantity
         payload = f'entry.1975053655={manager}&entry.901466373={articul}&entry.401979653={product}&entry.276639414={base_price}&entry.1723905293={price}&entry.1073455884={quantity}&entry.1287285077={total}&entry.455948029={customer}&entry.665447278={notes}'
         payload = quote_plus(payload, safe=';/?:@&=+$,')
         send_form(payload)
